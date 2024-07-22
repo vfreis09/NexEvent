740a73d7c934d524327df0d9d069404155d91369
vfreis09 <reis.vicente27@gmail.com>
2024-07-22 14:35:43 -0300

    user login added

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -18,8 +18,49 @@ const signup = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    // Query the database for the user with the given email
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    // Check if user exists
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const user = result.rows[0];
+
+    // Ensure the user object has a password field
+    if (!user.password) {
+      return res
+        .status(500)
+        .json({ message: "Password field is missing for this user" });
+    }
+
+    console.log(password, user.password);
+
+    // Compare the provided password with the stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // If the password does not match, return an error
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // If the password matches, respond with user details (excluding the password)
+    res.status(200).json({ email: user.email, id: user.id });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 const userController = {
   signup,
+  login,
 };
 
 export default userController;
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/userController";
 const router = Router();
 
 router.post("/signup", userController.signup);
+router.post("/login", userController.login);
 
 export default router;
