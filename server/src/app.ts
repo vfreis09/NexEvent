e988c8d71c6bd6fc34866c678e52b9fdbe8ebd0c
vfreis09 <reis.vicente27@gmail.com>
2024-07-22 13:58:13 -0300

    db and user table created

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import initDb from "./models/db";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+initDb()
+  .then(() => {
+    console.log("Database initialized");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.error("Database initialization failed:", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
-
-app.listen(3000);
---
+++ server/src/models/db.ts
@@ -0,0 +1,25 @@
+const pool = require("../config/dbConfig");
+
+const initDb = async () => {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        name VARCHAR(255),
+        bio TEXT,
+        role VARCHAR(10) DEFAULT 'standard',
+        contact TEXT,
+        visibility VARCHAR(7) DEFAULT 'public'
+    )
+  `;
+
+  try {
+    await pool.query(createTableQuery);
+    console.log("Users table created successfully or already exists.");
+  } catch (err) {
+    console.error("Error creating users table:", err);
+  }
+};
+
+export default initDb;
