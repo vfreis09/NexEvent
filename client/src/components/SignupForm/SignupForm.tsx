f620b0b78082107a5e56a7ab8716353a7d373772
vfreis09 <reis.vicente27@gmail.com>
2024-12-07 23:15:39 -0300

    navbar refresh signup bug fixed

--- client/src/components/SignupForm/SignupForm.tsx
+++ client/src/components/SignupForm/SignupForm.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useUser } from "../../context/UserContext";
 
 const SignupForm: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const { setUser, setIsLoggedIn } = useUser();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -23,7 +25,9 @@ const SignupForm: React.FC = () => {
         throw new Error("Signup failed");
       }
 
-      await response.json();
+      const userData = await response.json();
+      setUser(userData);
+      setIsLoggedIn(true);
       navigate("/");
     } catch (error) {
       console.error("Signup failed", error);
