4e519242b85f8411300438beac567e925b564b49
vfreis09 <reis.vicente27@gmail.com>
2025-03-01 20:46:03 -0300

    docker implementation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -6,7 +6,7 @@ const { Pool } = require("pg");
 
 export const pool = new Pool({
   user: process.env.USER,
-  host: "localhost",
+  host: "pgsql",
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: 5432,
