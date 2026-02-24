6bda124576ddb94f08a01007f601e61309c1b7ef
vfreis09 <reis.vicente27@gmail.com>
2026-02-23 22:44:19 -0300

    db config changed for prod

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -1,16 +1,15 @@
 import dotenv from "dotenv";
 import path from "path";
+const { Pool } = require("pg");
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-const { Pool } = require("pg");
-
 const pool = new Pool({
-  user: process.env.DB_USERNAME || "postgres",
-  host: process.env.DB_HOST || "127.0.0.1",
-  database: process.env.DB_DATABASE || "event-management",
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5434,
+  connectionString: process.env.DATABASE_URL,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
