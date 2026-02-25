653350d13136ae4f8167ea1f492bf57d8f69c7f3
vfreis09 <reis.vicente27@gmail.com>
2026-02-25 14:34:11 -0300

    google oauth env variables bug fixed

--- client/src/config/oauthConfig.ts
+++ client/src/config/oauthConfig.ts
@@ -8,14 +8,22 @@ interface OAuthClientConfig {
 const getEnvVar = (key: string): string => {
   const value = import.meta.env[key];
   if (!value) {
+    if (key === "VITE_PUBLIC_GOOGLE_AUTH_ENDPOINT")
+      return "https://accounts.google.com/o/oauth2/v2/auth";
     throw new Error(`Environment variable ${key} is not set!`);
   }
   return value;
 };
 
+const isLocal = window.location.hostname === "localhost";
+const prodRedirect =
+  "https://nexevent-app-production.up.railway.app/api/user/google/callback";
+const localRedirect = "http://localhost:3000/api/user/google/callback";
+
 const OAUTH_CONFIG: OAuthClientConfig = {
   CLIENT_ID: getEnvVar("VITE_PUBLIC_GOOGLE_CLIENT_ID"),
-  REDIRECT_URI: getEnvVar("VITE_PUBLIC_GOOGLE_REDIRECT_URI"),
+
+  REDIRECT_URI: isLocal ? localRedirect : prodRedirect,
 
   SCOPES: getEnvVar("VITE_PUBLIC_GOOGLE_SCOPES")
     .split(",")
